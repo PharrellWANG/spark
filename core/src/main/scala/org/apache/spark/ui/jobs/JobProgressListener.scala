f497e8699eb4db0223d649a5edb0e222c07e944a
Yuming Wang <wgyumg@gmail.com>
2017-01-10 14:51:41 +0800

    try to fix

--- core/src/main/scala/org/apache/spark/ui/jobs/JobProgressListener.scala
+++ core/src/main/scala/org/apache/spark/ui/jobs/JobProgressListener.scala
@@ -409,9 +409,8 @@ class JobProgressListener(conf: SparkConf) extends SparkListener with Logging {
 
       // If Tasks is too large, remove and garbage collect old tasks
       if (stageData.taskData.size > retainedTasks) {
-        val start = System.currentTimeMillis()
-        stageData.taskData = stageData.taskData.drop(stageData.taskData.size - retainedTasks)
-        logInfo(s"Time consuming: ${System.currentTimeMillis() - start}")
+        stageData.taskData = stageData.taskData.drop(
+          stageData.taskData.size - retainedTasks + (retainedTasks * 0.01).toInt)
       }
 
       for (
