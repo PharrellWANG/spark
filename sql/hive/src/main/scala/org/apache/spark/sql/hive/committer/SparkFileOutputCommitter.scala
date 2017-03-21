d7eff44e8b0571184a531d096f7ce0cc02e6ba8f
Yuming Wang <wgyumg@gmail.com>
2017-03-21 18:57:28 +0800

    try

--- sql/hive/src/main/scala/org/apache/spark/sql/hive/committer/SparkFileOutputCommitter.scala
+++ sql/hive/src/main/scala/org/apache/spark/sql/hive/committer/SparkFileOutputCommitter.scala
@@ -17,7 +17,9 @@
 
 package org.apache.spark.sql.hive.committer
 
-import org.apache.hadoop.fs.Path
+import java.io.IOException
+
+import org.apache.hadoop.fs.{FileStatus, FileSystem, Path}
 import org.apache.hadoop.mapreduce.{JobContext, TaskAttemptContext}
 import org.apache.hadoop.mapreduce.lib.output.FileOutputCommitter
 
@@ -30,6 +32,44 @@ class SparkFileOutputCommitter(outputPath: Path, context: TaskAttemptContext)
   extends FileOutputCommitter(outputPath, context) with Logging {
 
   override def commitJob(jobContext: JobContext): Unit = {
-    super.commitJob(jobContext)
+
+    if (hasOutputPath) {
+      val finalOutput: Path = outputPath
+      val fs: FileSystem = finalOutput.getFileSystem(context.getConfiguration)
+//      for (stat <- getAllCommittedTaskPaths(context)) {
+//        mergePaths(fs, stat, finalOutput)
+//      }
+      val taskPath = getCommittedTaskPath(context)
+      mergePaths(fs, taskPath, finalOutput)
+      cleanupJob(context)
+      if (context.getConfiguration.getBoolean(FileOutputCommitter.SUCCESSFUL_JOB_OUTPUT_DIR_MARKER,
+        true)) {
+        val markerPath: Path = new Path(outputPath, FileOutputCommitter.SUCCEEDED_FILE_NAME)
+        fs.create(markerPath).close
+      }
+    } else {
+      logWarning("Output Path is null in commitJob()")
+    }
   }
+
+  override def commitTask(context: TaskAttemptContext, taskAttemptPath: Path): Unit = {
+    if (hasOutputPath()) {
+      context.progress()
+    }
+  }
+
+  def hasOutputPath(): Boolean = outputPath != null
+
+  def mergePaths(fs: FileSystem, from: Path, to: Path): Unit = {
+    logDebug(s"Merging data from $from to $to")
+      if (fs.exists(to)) {
+        if (!fs.delete(to, true)) {
+          throw new IOException(s"Failed to delete ${to}")
+        }
+      }
+      if (!fs.rename(fs.getFileStatus(from).getPath, to)) {
+        throw new IOException("Failed to rename " + from + " to " + to)
+      }
+  }
+
 }
